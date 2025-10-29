c5f2aacc53fc36055e2c76308a65336547495b03
Jay Salway <2512956@leedstrinity.ac.uk>
2025-10-29 13:48:58 +0000

    Refactor MathJax configuration to remove duplicate settings and improve clarity

--- docs/assets/mathjax-config.js
+++ docs/assets/mathjax-config.js
@@ -1,6 +1,17 @@
 // Configure MathJax before the library loads so we can control when typesetting happens
 window.MathJax = {
   tex: {
+    // support common inline delimiters
+    inlineMath: [['$', '$'], ['\\(', '\\)']]
+  },
+  startup: {
+    // disable automatic typeset so we can re-run typesetting after client-side navs
+    typeset: false
+  }
+};
+// Configure MathJax before the library loads so we can control when typesetting happens
+window.MathJax = {
+  tex: {
     inlineMath: [['$', '$'], ['\\(', '\\)']],
     displayMath: [['$$','$$'], ['\\[','\\]']]
   },
