49b24c4ac97163abdec2db5e82a2033437b3c5a4
Jay Salway <2512956@leedstrinity.ac.uk>
2025-11-13 15:54:22 +0000

    Update site URL to reflect the correct domain for csref.dev

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'csref.dev',
   tagline: 'Course notes and modules',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://csref.dev',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -16,8 +16,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'jaysalw', // Usually your GitHub org/user name.
+  projectName: 'csref.dev', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
@@ -27,8 +27,7 @@ const config = {
     locales: ['en'],
   },
 
-  // add mermaid plugin and theme, and KaTeX stylesheet
-  plugins: ['docusaurus-plugin-mermaid'],
+  // add mermaid theme and KaTeX stylesheet
   themes: ['@docusaurus/theme-mermaid'],
 
   stylesheets: [
